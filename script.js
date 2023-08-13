3799c29823f7b2f748ec66566c9077ab00bcdad0
w̶̡͓̬̯̯͉͇̺̄͊̊́͗̄͜h̴̦̠̊̿̑̉̍́͘̚o̷̘͕̯̪̬̔̀̽͠ͅ ̴̨̯̠̞͙͚͓̰͍́a̷̡̡̧̤̥̭͉͕̬̘̗͗͋̇̆́̾̎͊̾́̕̕͝m̷̨̪̙̦̤͖̪̟̟̹̯͔͍̪̖̭̻̾͐̄̈́ ̶̞͉̭͈̘̦̥͍̝͖̺͕͎͈̣̝͔̀̀̂̈̾̈́̀͗̄̚i̷̛̬̟̭͚̝̝̮͓̻͍͎͉͆̀͗͗̏̏̀̏̋̈̆̓̌́͋̋ <74172849+PH-Apolonio@users.noreply.github.com>
2023-08-13 18:03:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function constructSearchQuery(targetUrl, dorkType) {
     findsubdomains: `https://www.google.com/search?q=site:%2A.${encodeURIComponent(targetUrl)}`,
     securityheaders: "site:" + targetUrl,
     xss: "site:" + targetUrl + " inurl:q OR inurl:s OR inurl:search OR inurl:id OR inurl:lang OR inurl:keyword OR inurl:query OR inurl:page OR inurl:view Or inurl:type OR inurl:url ",
-    bxss: "site:" + targetUrl + " intitle:contact.php OR intitle:contact us OR intitle:contactus.php OR intitle:contactus.aspx OR intitle:contactus.asp OR intitle:contactus.html OR intitle:contact-us.html OR intitle:contact_us.html OR intitle:contact.html OR intitle:contactus.html OR inurl:feedback OR inurl:Send Us a Message OR intitle:Send Us a Message OR intitle:support OR intext:Please choose a request type below OR inurl:submit ",
+    bxss: "site:" + targetUrl + "  intitle:contact OR  inurl:feedback OR inurl:Send Us a Message OR intitle:Send Us a Message OR intitle:support OR intext:Please choose a request type below OR inurl:submit ",
     sqli: "site:" + targetUrl + " inurl:php?id= OR inurl:php ",
    
     
